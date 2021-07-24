bb5775b14f985e79252d5f348d80c95ec1bbf530
Hatice <haticekaraty@gmail.com>
2021-07-23 23:30:41 -0400

    Done.

--- main.js
+++ main.js
@@ -2,10 +2,34 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
+let heartButtons = document.querySelectorAll(".like-glyph");
 
+heartButtons.forEach(heart => {
+  heart.addEventListener("click",(event) => {
+    mimicServerCall()
+    .then(response => handleLikes(event.target))
+    .catch(error => {
+      console.log(error)
+      let modal = document.querySelector("#modal")
+      let errorMessage = document.querySelector("#modal-message")
+      errorMessage.innerText = error
+      modal.classList.remove("hidden");
+      setTimeout(function(){
+        modal.classList.add("hidden");
+      },5000)
+    })
+  })
+});
 
-
+function handleLikes(heart) {
+  if (heart.innerText === EMPTY_HEART){
+    heart.innerText = FULL_HEART
+    heart.classList.add("activated-heart");
+  } else {
+    heart.innerText = EMPTY_HEART
+    heart.classList.remove("activated-heart");
+  }
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
